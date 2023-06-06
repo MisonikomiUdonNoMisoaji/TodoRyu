55d1ec1ba93a6ac6ce8a152369a1ebe5736c2cce
Ryu-nakay <ryukenkana@gmail.com>
2023-06-06 21:24:26 +0900

    リストの表示

--- app/src/main/java/com/example/todoryu/MainActivity.kt
+++ app/src/main/java/com/example/todoryu/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.todoryu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val array = arrayOf("リスト1", "リスト2", "リスト3")
+        val listView = findViewById<ListView>(R.id.list_view)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array)
+        listView.adapter = adapter
     }
 }
